891b7fbc9ee0f54daae0aa3056e348cf72ca7eed
Mitali <mitali9205@gmail.com>
2024-08-12 22:48:22 +0530

    fix: minor changes

--- client/src/api/productApi.js
+++ client/src/api/productApi.js
@@ -4,7 +4,7 @@ export const getProduct = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5003/api/products")
+    fetch("https://backend-ikk2.onrender.com/api/products")
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
