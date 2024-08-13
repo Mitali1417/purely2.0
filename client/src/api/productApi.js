c4cd7fee252b17719271332d38445acb0dbb8b44
Mitali <mitali9205@gmail.com>
2024-08-13 08:09:36 +0530

    fix: minor change in productApi

--- client/src/api/productApi.js
+++ client/src/api/productApi.js
@@ -4,7 +4,7 @@ export const getProduct = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://backend-ikk2.onrender.com/api/products")
+    fetch("https://purely2-0-backend.onrender.com/api/products")
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
