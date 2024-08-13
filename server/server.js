2a7c87ea5cddd61fb6ac4dd76a763abad1267ff3
Mitali <mitali9205@gmail.com>
2024-08-13 07:36:15 +0530

    fix: minor change in server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5003;
 
