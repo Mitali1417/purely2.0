af38f9330be577fa24866bd6fd584d3008256a4d
Mitali <mitali9205@gmail.com>
2025-08-19 21:17:48 +0530

    server-copy removed

--- server-copy/routes/userQueryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitQuery } = require('../controllers/userQueryController');
-
-router.post('/submit', submitQuery);
-
-module.exports = router;
