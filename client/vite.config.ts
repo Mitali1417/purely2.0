49589725e3f43ea2cb08653f79da79ad3cfcea44
Mitali <mitali9205@gmail.com>
2025-09-12 19:17:49 +0530

    fix: vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  root: 'client',  // specify that index.html is inside client/
+  plugins: [react()],
+  build: {
+    outDir: '../dist',  // output folder relative to root
+  }
+});
